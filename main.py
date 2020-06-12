16e072b793578d27e43a021f82c318324ed9434c
remilepriol <remi.lp.17@gmail.com>
2020-06-12 01:01:20 -0400

    Embed bokeh plots into the main html file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import bokeh
 import numpy as np
-from bokeh import io, layouts, models
+from bokeh import io, layouts, models, embed
 
 from plotting import plot_conjugate
 
@@ -31,24 +31,36 @@ functiondict = {
     'x**4': (lambda x: x ** 4, -1, 1),
     'x**4 + 4 x**3': (lambda x: x ** 4 + 4 * x ** 3, -1, 1),
     'x**2 - cos(2 x)': (lambda x: x ** 2 - np.cos(2 * x), -5, 5),
-    #'sin(x)': (np.sin, -np.pi, 0),
-    #'Entropy(x) = x log(x) + (1-x) log(1-x)': (entropy, 0, 1),
-    #'x sin(1/x)': (lambda x: x * np.sin(1 / x), -1, 1),
-    #'0': (np.zeros_like, -1, 1), # not working yet
+    # 'sin(x)': (np.sin, -np.pi, 0),
+    # 'Entropy(x) = x log(x) + (1-x) log(1-x)': (entropy, 0, 1),
+    # 'x sin(1/x)': (lambda x: x * np.sin(1 / x), -1, 1),
+    # '0': (np.zeros_like, -1, 1), # not working yet
     # 'Hinge(x) = max(0,1-x)': (hinge, -2, 2),
     # 'Hinge(x)**2': (squared_hinge, -2, 2),
     # 'Random Polynomial of degree 6': (random_polynomial(6), -2, 2),
 }
 
-abstract=''
+# abstract=''
 # with open('abstract.txt', 'r') as file:
 #     abstract = file.read()
+# figlist = [models.Div(text='<h1>Convex Conjugate Visualization</h1> \n' + abstract)]
 
-figlist = [models.Div(text='<h1>Convex Conjugate Visualization</h1> \n' + abstract)]
+figlist = []
 for name, (func, inf, sup) in functiondict.items():
     figlist += [layouts.column(
         models.Div(text=f'<h2>{name}</h2>'),
         plot_conjugate(func, np.linspace(inf, sup, 150))
     )]
+finalplot = layouts.column(figlist)
 
-bokeh.io.save(layouts.column(figlist), title='conjugates', filename='conjugate.html', )
+script, div = embed.components(finalplot)
+
+with open('abstract_math.html','r') as fin:
+    htmlstring = fin.read()
+
+newhtmlstring = htmlstring.replace("<div>PLOTSHOLDER</div>", div).replace("<div>SCRIPTHOLDER</div>", script)
+
+with open("conjugate.html","w") as fout:
+    fout.write(newhtmlstring)
+
+# bokeh.io.save(finalplot, title='conjugates', filename='conjugate.html', )
