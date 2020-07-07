4ec3a537abb6e72f523720f6706d36f6098a3c7d
remilepriol <remi.lp.17@gmail.com>
2020-07-07 16:59:46 -0400

    Create radio buttons with bokeh

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
 import bokeh
-import numpy as np
 from bokeh import embed, io
 
 from plotting import plot_conjugate
-
-
-def entropy(x):
-    return x * np.log(np.maximum(x, 1e-50)) + (1 - x) * np.log(np.maximum(1 - x, 1e-50))
-
-
-def hinge(x):
-    return np.maximum(1 - x, 0)
-
-
-def squared_hinge(x):
-    return np.maximum(1 - x, 0) ** 2
-
-
-def random_polynomial(degree=4):
-    coeffs = .3 * np.random.randn(degree + 1)
-    coeffs[-1] = np.absolute(coeffs[-1])
-    return np.polynomial.polynomial.Polynomial(coeffs)
 
 
 bokeh.io.output_file(filename='conjugate.html', title='conjugate')
